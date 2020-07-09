702c939500b0adac96105b148d9e76aaea5e190c
Giovane Pianco <giovanepianco@MacBook-Pro.local>
2020-07-09 18:45:45 -0300

    Reuniao controller

---
+++ Reunioes/Controllers/ReuniaoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Reunioes.Dominio.Contratos;
+using Reunioes.Dominio.Entidades;
+
+namespace Reunioes.Web.Controllers
+{
+
+    [Route("api/[Controller]")]
+    public class ReuniaoController : Controller
+    {
+        private readonly IReuniaoRepositorio _reuniaoRepositorio;
+        public ReuniaoController(IReuniaoRepositorio reuniaoRepositorio)
+        {
+            _reuniaoRepositorio = reuniaoRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_reuniaoRepositorio.ObterTodos());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Reuniao reuniao)
+        {
+            try
+            {
+                _reuniaoRepositorio.Adicionar(reuniao);
+                return Created("api/reuniao", reuniao);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
