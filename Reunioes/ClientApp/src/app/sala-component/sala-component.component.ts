a14647c1baf74043cf56078a5130a0e4ddbc5b49
GIOVANE PIANCO <gopianco@hotmail.com>
2020-07-10 11:18:14 -0300

    criacao salacomponent

---
+++ Reunioes/ClientApp/src/app/sala-component/sala-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sala-component',
+  templateUrl: './sala-component.component.html',
+  styleUrls: ['./sala-component.component.css']
+})
+export class SalaComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
