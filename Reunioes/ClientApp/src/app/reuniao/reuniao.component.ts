33741b81d5f989ddc02b64c75acfb254ef05b803
Giovane Pianco <giovanepianco@MacBook-Pro.local>
2020-07-12 13:27:54 -0300

    cadastro reuniao

---
+++ Reunioes/ClientApp/src/app/reuniao/reuniao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reuniao',
+  templateUrl: './reuniao.component.html',
+  styleUrls: ['./reuniao.component.css']
+})
+export class ReuniaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
