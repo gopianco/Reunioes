6601cc0978081bb475f2487b78fe23eaf1bfea37
Giovane Pianco <giovanepianco@MacBook-Pro.local>
2020-07-11 22:34:48 -0300

    servico cadastro e consulta de salas

---
+++ Reunioes/ClientApp/src/app/services/sala.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, Inject, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Sala } from '../model/sala';
+import { StaticClassProvider } from '@angular/core/src/di/provider';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService implements OnInit{
+
+  private _baseURL: string;
+  private salas: Sala[];
+
+  get headers():HttpHeaders{
+    return new HttpHeaders().set('content-type', 'application/json');
+  }
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) { 
+
+    this._baseURL = baseUrl;
+  }
+
+  ngOnInit(): void {
+    this.salas = [];
+  }
+
+  public cadastrar(sala: Sala): Observable<Sala> {
+
+    return this.http.post<Sala>(this._baseURL + '', JSON.stringify(sala), { headers: this.headers });
+  }
+
+  public obterTodos(sala: Sala): Observable<Sala[]> {
+    return this.http.get<Sala[]>(this._baseURL + 'api/sala');
+  }
+
+
+}
