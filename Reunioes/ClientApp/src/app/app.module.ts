6d445a48c8305a1b660bf65f2fafa87f3b566623
gopianco <gopianco@hotmail.com>
2020-07-14 17:46:33 -0300

    remoção de componentes de exemplo

--- Reunioes/ClientApp/src/app/app.module.ts
+++ Reunioes/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { SalaComponent } from './sala/sala.component';
 import { SalaService } from './services/sala.service';
 import { ReuniaoService } from './services/reuniao.service';
@@ -21,8 +19,7 @@ import { ListarReunioesComponent } from './listar-reunioes/listar-reunioes.compo
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
+  
     SalaComponent,
     ReuniaoComponent,
     ListarReunioesComponent,
@@ -34,8 +31,7 @@ import { ListarReunioesComponent } from './listar-reunioes/listar-reunioes.compo
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+     
       { path: 'sala', component: SalaComponent },
       { path: 'reuniao', component: ReuniaoComponent },
       { path: 'listar-reunioes', component: ListarReunioesComponent },
